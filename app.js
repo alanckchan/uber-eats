3d50f9dd0d84619c78a34770936b1733caf9e882
Sumit Sahni <susahni@gmail.com>
2014-02-04 22:36:57 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = module.exports = express();
+
+/**
+* Configuration
+*/
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
+
+// development only
+if (app.get('env') === 'development') {
+   app.use(express.errorHandler());
+};
+
+// production only
+if (app.get('env') === 'production') {
+  // TODO
+}; 
+
+// Routes
+app.get('/', routes.index);
+// redirect all others to the index (HTML5 history)
+app.get('*', routes.index);
+
+/**
+* Start Server
+*/
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
