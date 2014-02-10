e260aa7e483232a4a3cf5b6c874231bb24b30e71
Sumit Sahni <susahni@gmail.com>
2014-02-10 01:10:30 -0500

    Renamed gruntfile.js to Gruntfile.js for heroku buildpack to work

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,10 @@
 'use strict';
 
 module.exports = function(grunt) {
-  grunt.loadTasks('tasks');
-
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json')
   });
+
+  grunt.loadTasks('tasks');
+  grunt.registerTask('heroku:production', 'db-seed-data');
 };
